ee353779cabe5a35d773fac39b8d34a3d5507c54
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-02-27 19:58:53 -0300

    função vs objeto

--- exercicios-js/fundamentos/objeto2.js
+++ exercicios-js/fundamentos/objeto2.js
@@ -5,6 +5,6 @@ const Cliente = function() {};
 console.log(typeof Cliente);
 console.log(typeof new Cliente);
 
-class Produto {};
+class Produto {}; //ES 2015 (ES6)
 console.log(typeof Produto);
-console.log(typeof new Produto);
+console.log(typeof new Produto());
